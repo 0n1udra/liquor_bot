9bdd0bc73a0d04eb68beb1fdda813a7e5e810793
0n1udra <dt01@pm.me>
2022-06-28 04:44:47 -0400

    test.py

--- source/test.py
+++ source/test.py
@@ -1,2 +1,20 @@
-x = int('hi')
-print("OK")
+import requests, bs4
+url = 'https://ice.liquor.nh.gov/public/default.asp?Category=inquiries&Service=prodfindconf'
+session = requests.session().get(url)
+#session.get(url)
+keys = 'titos'
+results = session.post(url, data={'REQ':keys})
+soup = bs4.BeautifulSoup(results.text, 'html.parser')
+tds = []
+if table := soup.find_all('tr')[1]:
+    for td in table.find_all('td'):
+        try: int(td.contents[0].split(' ')[0])
+        except: continue
+        else: tds.append(td.contents[0])
+
+    print(tds)
+
+else:
+    print("ERR")
+    exit()
+
